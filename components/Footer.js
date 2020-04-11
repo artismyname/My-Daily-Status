3251d28fe2b338c9cd8b5a1a7eb03723138ba86f
artismyname <4lex4andre@gmail.com>
2020-04-11 03:31:52 -0500

    initial commit

---
+++ components/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Footer = () => {
+    return (
+        <div className='py-4 text-center bg-gray-400'>
+            MyDailyStatus é um projeto criado durante o Fullstack Lab do DevPleno.
+            <br />
+            implementado por:{' '}
+            <a href="https://linkedin.com/in/AlexandreMerched">Alexandre Merched</a>
+        </div>
+    )
+}
+
+export default Footer
